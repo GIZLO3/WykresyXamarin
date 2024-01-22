687ff38f33008ef43e0d8e5c2238f5b2c202d419
Michał Giza <m.giza12345@gmail.com>
2024-01-22 15:33:43 +0100

    create EditChartData page

---
+++ WykresyXamarin/WykresyXamarin/EditChartDataPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WykresyXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditChartDataPage : ContentPage
+    {
+        public EditChartDataPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
