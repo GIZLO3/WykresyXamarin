17245739dc21838368d4e7eb34fa0db723becbef
Michał Giza <m.giza12345@gmail.com>
2024-01-22 15:22:08 +0100

    add static ChartTitle property

--- WykresyXamarin/WykresyXamarin/MainPage.xaml.cs
+++ WykresyXamarin/WykresyXamarin/MainPage.xaml.cs
@@ -11,8 +11,9 @@ namespace WykresyXamarin
     public partial class MainPage : TabbedPage
     {
         public static List<ChartData> ChartData { get; set; }
+        public static string ChartTitle { get; set; }
         private Color[] Colors = { Color.Red, Color.Blue, Color.Orange, Color.Green, Color.Purple, Color.Gray };
-
+        
         public MainPage()
         {
             InitializeComponent();
