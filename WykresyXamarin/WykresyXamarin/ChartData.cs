a2c789ff7dc8102391c59494f2033dedb7e8dabf
Michał Giza <m.giza12345@gmail.com>
2024-01-22 15:19:09 +0100

    create ChartData class

---
+++ WykresyXamarin/WykresyXamarin/ChartData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WykresyXamarin
+{
+    public class ChartData
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+}
